b1a3e67992886b202ab570b5c147da797a3860f5
Chek <chek214@live.com>
2021-04-08 13:03:58 -0500

    adding express

--- main.js
+++ main.js
@@ -7,12 +7,34 @@ const puppeteerOptions = {
     }
 }
 
+var express = require('express')
+var app = express()
+var cors = require('cors')
+
+
+var qrcod = ''
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', function(req, res, next){
+})
+
+app.get('/qr', function(req, res, next){
+    res.send(JSON.stringify(qrcod))
+})
+
+app.post('/sendmsg', function(req, res, next){
+})
+
+
 
 const { Client } = require('whatsapp-web.js')
 const client = new Client(puppeteerOptions)
 
 client.on('qr', qr => {
     qrcode.generate(qr, {small: true})
+    qrcod = qr
 })
 
 client.on('ready', () => {
@@ -20,3 +42,5 @@ client.on('ready', () => {
 })
 
 client.initialize()
+
+app.listen(3200)
